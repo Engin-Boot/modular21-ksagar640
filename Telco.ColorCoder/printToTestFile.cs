c31b1207d15fbd2c01373f7cf749c3f407f6ff56
Sagar Jaiswal <sagar.jaiswal@publicissapient.com>
2021-07-25 17:16:10 +0530

    Added assert in testing files

--- Telco.ColorCoder/printToTestFile.cs
+++ Telco.ColorCoder/printToTestFile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Drawing;
+using System.Diagnostics;
 namespace ColorCoder{
 
 	public class printToTestFile : Iprintable{
@@ -16,7 +17,10 @@ namespace ColorCoder{
 	            {
 	                for (int j = 0; j < minor.Length; j++)
 	                {
+	                	ColorPair Color = (new GetColor(pairnum)).GetColorFromPairNumber();
 	                    sw.WriteLine("Pair Number: {0} ,Colors: major{1} - minor{2}", pairnum, major[i], minor[j]);
+	                    Debug.Assert(Color.majorColor == coordinator.colorMapMajor[i]);
+	                    Debug.Assert(Color.minorColor == coordinator.colorMapMinor[j]);
 	                    pairnum++;
 	                }
 	            }
